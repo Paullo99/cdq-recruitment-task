d706f4992feac2e78d538274365e6e63d4bd138c
Paweł Maciończyk <macionczyk.paw@gmail.com>
2025-04-08 15:26:29 +0200

    Initial commit

---
+++ src/main/java/com/cdq/recruitmenttask/CdqRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.cdq.recruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdqRecruitmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdqRecruitmentTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdq/recruitmenttask/CdqRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdq.recruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdqRecruitmentTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
