0d4d950ea2d9cf890d1385a2f559f20537a07a72
Paweł Maciończyk <macionczyk.paw@gmail.com>
2025-04-14 15:39:31 +0200

    improve function readability

--- src/main/java/com/cdq/recruitmenttask/util/FieldComparator.java
+++ src/main/java/com/cdq/recruitmenttask/util/FieldComparator.java
@@ -2,6 +2,7 @@ package com.cdq.recruitmenttask.util;
 
 import com.cdq.recruitmenttask.dto.FieldChangeResult;
 import com.cdq.recruitmenttask.model.FieldChangeClassification;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.similarity.LevenshteinDistance;
 
 public class FieldComparator {
@@ -10,30 +11,44 @@ public class FieldComparator {
     private static final double THRESHOLD_HIGH = 0.9;
 
     public static FieldChangeResult compare(String field, String oldVal, String newVal) {
-        if (isEmpty(oldVal) && !isEmpty(newVal)) {
+        if (isAdded(oldVal, newVal)) {
             return new FieldChangeResult(field, null, newVal, FieldChangeClassification.ADDED);
         }
 
-        if (!isEmpty(oldVal) && isEmpty(newVal)) {
+        if (isDeleted(oldVal, newVal)) {
             return new FieldChangeResult(field, oldVal, null, FieldChangeClassification.DELETED);
         }
 
-        if (isEmpty(oldVal) && isEmpty(newVal)) {
+        if (isUnchangedEmpty(oldVal, newVal)) {
             return new FieldChangeResult(field, oldVal, newVal, FieldChangeClassification.HIGH);
         }
 
         double similarity = calculateSimilarity(oldVal, newVal);
-        FieldChangeClassification classification;
-
-        if (similarity < THRESHOLD_LOW) {
-            classification = FieldChangeClassification.LOW;
-        } else if (similarity < THRESHOLD_HIGH) {
-            classification = FieldChangeClassification.MEDIUM;
-        } else {
-            classification = FieldChangeClassification.HIGH;
-        }
+        FieldChangeClassification classification = classifyBySimilarity(similarity);
 
         return new FieldChangeResult(field, oldVal, newVal, classification);
+    }
+
+    private static boolean isAdded(String oldVal, String newVal) {
+        return StringUtils.isEmpty(oldVal) && !StringUtils.isEmpty(newVal);
+    }
+
+    private static boolean isDeleted(String oldVal, String newVal) {
+        return !StringUtils.isEmpty(oldVal) && StringUtils.isEmpty(newVal);
+    }
+
+    private static boolean isUnchangedEmpty(String oldVal, String newVal) {
+        return StringUtils.isEmpty(oldVal) && StringUtils.isEmpty(newVal);
+    }
+
+    private static FieldChangeClassification classifyBySimilarity(double similarity) {
+        if (similarity < THRESHOLD_LOW) {
+            return FieldChangeClassification.LOW;
+        } else if (similarity < THRESHOLD_HIGH) {
+            return FieldChangeClassification.MEDIUM;
+        } else {
+            return FieldChangeClassification.HIGH;
+        }
     }
 
     private static double calculateSimilarity(String oldVal, String newVal) {
@@ -47,9 +62,5 @@ public class FieldComparator {
 
         double dissimilarity = (double) distance / maxLength;
         return 1.0 - dissimilarity;
-    }
-
-    private static boolean isEmpty(String val) {
-        return val == null || val.isBlank();
     }
 }
