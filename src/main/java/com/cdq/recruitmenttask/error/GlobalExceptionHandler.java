c473fe44935b85c6c139a7b92fb39c75dc2242e9
Paweł Maciończyk <macionczyk.paw@gmail.com>
2025-04-14 16:22:32 +0200

    improve the context of the comment

--- src/main/java/com/cdq/recruitmenttask/error/GlobalExceptionHandler.java
+++ src/main/java/com/cdq/recruitmenttask/error/GlobalExceptionHandler.java
@@ -2,12 +2,18 @@ package com.cdq.recruitmenttask.error;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Hidden //https://stackoverflow.com/questions/79274106/how-to-use-both-restcontrolleradvice-and-swagger-ui-in-spring-boot
+// Swagger tries to document @RestControllerAdvice classes by default.
+// This causes runtime issues in Spring Boot 3+ (NoSuchMethodError).
+// Adding @Hidden prevents Swagger from processing this class.
+// See: https://stackoverflow.com/questions/79274106/how-to-use-both-restcontrolleradvice-and-swagger-ui-in-spring-boot
+@Hidden
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -26,6 +32,7 @@ public class GlobalExceptionHandler {
                 ErrorCode.INTERNAL_SERVER_ERROR.name(),
                 ex.getMessage()
         );
+        log.error("Unexpected error occurred", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
