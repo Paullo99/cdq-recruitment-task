98a06b91e44c1f06d02651240047fe7e77495e88
Paweł Maciończyk <macionczyk.paw@gmail.com>
2025-04-14 23:00:10 +0200

    add target and source in TaskMapper

--- src/main/java/com/cdq/recruitmenttask/mapper/TaskMapper.java
+++ src/main/java/com/cdq/recruitmenttask/mapper/TaskMapper.java
@@ -3,9 +3,10 @@ package com.cdq.recruitmenttask.mapper;
 import com.cdq.recruitmenttask.dto.TaskSummaryResponse;
 import com.cdq.recruitmenttask.model.Task;
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
+    @Mapping(target = "taskId", source = "id")
     TaskSummaryResponse toTaskSummaryResponse(Task task);
 }
